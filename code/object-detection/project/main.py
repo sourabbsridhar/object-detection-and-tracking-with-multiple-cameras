7986efb09f99ea354798028138bafdf877f3c0ff
Apichai Sompolpong <bssompolpong@gmail.com>
2020-11-20 17:48:13 +0100

    Created a local library of YOLOv4 where we can modify
    - Added frame_id to each frame
    - Printing bboxes every frame

--- code/object-detection/project/main.py
+++ code/object-detection/project/main.py
@@ -1,34 +1,26 @@
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-import os
-import sys
-import tensorflow as tf
-from yolov4.tf import YOLOv4
 import cv2
+# from yolov4.tf import YOLOv4 # installed (global) library
+from py_src.yolov4.tf import YOLOv4 # local library: to use the modified library!
 
-# ''' Add data and weights directories to path '''
-# pathWeight = "weights"
-# pathData = "data"
-# os.environ['PATH'] += ':'+pathData
-# os.environ['PATH'] += ':'+pathWeight
-for p in sys.path:
-    print(p)
 
+'''Path to data'''
 dataPath = 'data/test_1.jpg'
 dataPathResized = 'data/test_1_resized.jpg'
+
 ''' Rescale images to 640 * 480'''
-img = cv2.imread(dataPath)
-width = 640
-height = 480
-dim = (width,height)
-resized = cv2.resize(img,dim,interpolation=cv2.INTER_AREA)
+# This is not necessary because the yolov4 library also has this function
+# img = cv2.imread(dataPath)
+# width = 640
+# height = 480
+# dim = (width,height)
+# resized = cv2.resize(img,dim,interpolation=cv2.INTER_AREA)
 # print("Resized dimensions: ", resized.shape)
 # cv2.imshow("Resized image", resized)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
-cv2.imwrite(dataPathResized, resized)
+# cv2.imwrite(dataPathResized, resized)
+''' End rescale'''
+
 
 yolo = YOLOv4()
 yolo.classes = "data/coco.names"
@@ -37,7 +29,6 @@ yolo.input_size = (640,480)
 yolo.make_model()
 yolo.load_weights("weights/yolov4.weights", weights_type = "yolo")
 
-# yolo.inference(media_path=dataPath)
+'''Infer image/video with IOU threshold = 0.5'''
 # yolo.inference(media_path="data/sample_vid_3_Trim.mp4", is_image=False,iou_threshold=0.5)
 yolo.inference(media_path=dataPath, is_image=False,iou_threshold=0.5)
-
