90769248963cf2c00272f929d945507cf288e447
Sourab Bapu Sridhar <sourabbsridhar@gmail.com>
2020-11-26 12:59:23 +0100

    initial placeholder implementation for Pose Estimation Class

--- code/pose_estimation.py
+++ code/pose_estimation.py
@@ -1,2 +1,22 @@
 # Implementation of the Pose Estimation Function (To be provided by the handover subgroup)
 
+import numpy as np
+
+class PoseEstimation:
+    def __init__(self, mode):
+        self.mode = mode
+
+    def OfflinePoseEstimation(self):
+        pass
+
+    def OnlinePoseEstimation(self):
+        pass
+
+    def EstimatePose(self):
+
+        if(self.mode == "online"):
+            self.OnlinePoseEstimation()
+        else:
+            self.OnlinePoseEstimation()
+
+    
