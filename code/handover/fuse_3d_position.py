c1212a5c7929da6c39b888e3f643d7cce1792185
Sourab Bapu Sridhar <sourabbsridhar@gmail.com>
2021-01-12 10:40:34 +0100

    Removed Unnecessary Files

--- code/handover/fuse_3d_position.py
+++
@@ -1,4 +0,0 @@
-# Implementation of Fusion Function to fuse 3D Image points based on Euclidean Distance (To be provided by the handover subgroup)
-
-def fuse3dPosition(projection3dPosition):
-    return 0
